76e3eecebfc95ebe34810276bceb59060c4f2774
Jason_Shu <shuyue524@gmail.com>
2019-03-17 21:46:28 +0800

    单向绑定2.0

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
+function observe(data) {
+    if(JSON.stringify(data) === '{}' || typeof data !== 'object') return;
+    for(let key in data) {
+        let val = data[key];
+        let subject = new Subject(); // 每一个key都是一个主题
+        Object.defineProperty(data, key, {
+            get: function() {
+                console.log(`get ${key}`)
+                if(currentObserver) {
+                    console.log('hasCurrentObserver');
+                    currentObserver.subscribeTo(subject);
+                }
+                return val;
+            },
+
+            set: function(newVal) {
+                val = newVal;
+                console.log('start notify....');
+                subject.notify();
+            }
+        })
+        if(typeof val === 'object') {
+            this.observe(val);
+        }
+    }
+}
+
 let id = 0;
 let currentObserver = null;
 
-// 主题类
 class Subject {
     constructor() {
         this.id = id++;
         this.observers = [];
     }
 
-    // 添加对象
     addObserver(observer) {
-        this.observers.push(observer)
+        let index = this.observers.indexOf(observer);
+        if(index === -1) {
+            this.observers.push(observer);
+        }
     }
 
-    // 移除对象
     removeObserver(observer) {
         let index = this.observers.indexOf(observer);
         if(index > -1) {
@@ -21,47 +48,42 @@ class Subject {
         }
     }
 
-    // 发布通知
     notify() {
         this.observers.forEach((observer) => {
-            observer.update()
+            observer.update();
         })
     }
 
 }
 
-
-// 对象类
 class Observer {
     constructor(vm, key, cb) {
-        this.subjects = {}; // 存放我们订阅的主题集合
+        this.subjects = {}; // 存放该observer订阅的主题
         this.vm = vm;
         this.key = key;
         this.cb = cb;
-        this.value = this.getValue(); // ?
+        this.value = this.getValue();
     }
 
-    // 更新
     update() {
-        let oldValue = this.value; //旧数据
-        let newValue = this.getValue(); // 更新前，先获取新数据
-        if(newValue !== oldValue) {
-            // 数据有更新
-            this.value = newValue;
-            this.cb.bind(this.vm)(newValue, oldValue);
+        let oldVal = this.value;
+        let newVal = this.getValue();
+        if(oldVal !== newVal) {
+            // 值有更改,要更新
+            this.value = newVal;
+            this.cb.bind(this.vm)(newVal, oldVal);
         }
     }
 
-    // 订阅
     subscribeTo(subject) {
         if(! this.subjects[subject.id]) {
-            console.log('subscribeTo..', subject);
+            // 如果我们还未订阅该主题
+            console.log('SubscribeTo ...', subject);
             subject.addObserver(this);
             this.subjects[subject.id] = subject;
         }
     }
 
-    // 获取当前key的value
     getValue() {
         currentObserver = this;
         let value = this.vm.$data[this.key];
@@ -71,88 +93,51 @@ class Observer {
 }
 
 
-// 监听函数
-function observe(data) {
-    if(!data || typeof data !== 'object') return;
-    for(let key in data) {
-        let value = data[key];
-        //每一个key就是一个subject
-        let subject = new Subject();
-        console.log(data, key)
-        Object.defineProperty(data, key, {
-            enumerable: true,
-            configurable: true,
-            get: function() {
-                console.log(`get ${key}: ${value}`);
-                if(currentObserver) {
-                    // 通过currentObserver这个全局变量来判断是否订阅主题
-                    console.log('has currentObserver');
-                    currentObserver.subscribeTo(subject);
-                }
-                return value;
-            },
-            set: function(newValue) {
-                value = newValue;
-                console.log('start notify...');
-                subject.notify();
-            }
-        })
-
-        if(typeof value === 'object') {
-            observe(value);
-        }
-
-    }
-}
-
-
-// MVVM类
 class MVVM {
-
     constructor(opts) {
         this.init(opts);
         observe(this.$data);
-        this.compile(); // 编译
+        // 编译
+        this.compile();
     }
 
     init(opts) {
         this.$el = document.querySelector(opts.el);
         this.$data = opts.data;
-        this.observers = [];
-
+        this.observes = [];
     }
 
     compile() {
+        // 转换视图层的节点
         this.traverse(this.$el);
     }
 
-    // 转换
     traverse(node) {
-        if(node.nodeType === 1) {
+        if(node.nodeType === 1) { // 如果为节点,就递归转换
             node.childNodes.forEach((childNode) => {
                 this.traverse(childNode);
             })
-        } else if(node.nodeType === 3) { //文本
-            this.renderText(node);
+        } else if(node.nodeType === 3){ //如果是文本
+            this.renderText(node); // 就渲染文字
         }
     }
 
     renderText(node) {
         let reg = /{{(.+?)}}/g;
         let match;
-
-        while(match = reg.exec(node.nodeValue)) {
+        while((match = reg.exec(node.nodeValue))) {
             let raw = match[0];
             let key = match[1].trim();
-
             node.nodeValue = node.nodeValue.replace(raw, this.$data[key]);
-
-            new Observer(this, key, function(newValue, oldValue) {
-                node.nodeValue = node.nodeValue.replace(oldValue, newValue)
+            // 针对每一个key，创建一个观察者
+            new Observer(this, key, function(val, oldVal) {
+                node.nodeValue = node.nodeValue.replace(oldVal, val);
             })
         }
     }
+
 }
+
 
 let vm = new MVVM({
     el: '#app',
@@ -161,3 +146,4 @@ let vm = new MVVM({
         age: 23
     }
 })
+
